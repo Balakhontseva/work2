0344463e1cb8bb47d1522e8c92e3cd09901b9a54
Balakhontseva <abalackhontseva@productengine.com>
2011-12-19 20:01:57 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Work1::Application.config.secret_token = '670c8d74d3e5c8e80af99f98af3704eedb7869f05f397e902178a799cc085bfaf2511ad9e80e557ae94aaaa567a684e3be74401f8b318a1caff15282cc0684aa'
